ea1665f2a924b67d9b5d701e94c1407b15145159
Edwin Rivera <waehake@gmail.com>
2018-02-28 09:45:17 -0500

    updated files for privacy

--- src/data/base.js
+++ src/data/base.js
@@ -2,12 +2,13 @@ import Rebase from 're-base';
 import firebase from 'firebase';
 
 var config = {
-    apiKey: 'AIzaSyD1QjaMp93d7Ryy1PqAMAGFsdKq52ksPyM',
-    authDomain: 'data-guestbook.firebaseapp.com',
-    databaseURL: 'https://data-guestbook.firebaseio.com',
-    projectId: 'data-guestbook',
-    storageBucket: '',
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+};
 
 const app = firebase.initializeApp(config);
 const base = Rebase.createClass(app.database());
