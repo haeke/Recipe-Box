fec96550d478fea038c2f02cc607977fd6d636a3
Edwin Rivera <waehake@gmail.com>
2018-03-08 18:56:50 -0500

    added form validation to addrecipe and editrecipe so you cannot leave the form fields blank, a user will be notified and the state will not be updated

--- src/components/AddRecipe.js
+++ src/components/AddRecipe.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
-import { Modal, Button, Form } from 'semantic-ui-react';
+import { Modal, Button, Form, Message } from 'semantic-ui-react';
 
 class AddRecipe extends Component {
     constructor(props) {
         super(props);
+        //set the initial state for the addRecipe modal
         this.state = {
             open: false,
             name: '',
             description: '',
+            error: false,
         }
 
         this.show = this.show.bind(this);
@@ -15,23 +17,34 @@ class AddRecipe extends Component {
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
     }
-
+    //set the state of open to true and size of the modal to be small
     show = size => () => this.setState({ size, open: true });
-
+    //set the state of open to false to close the modal
     close = () => this.setState({ open: false });
 
+    //update the input/ state object when a user types
     handleChange = (e, { name, value }) => this.setState({[name]: value});
 
+    //send the text entered into the 
     handleSubmit = () => {
         const { name, description } = this.state;
-        //create an object containing the name and description
-        const newItem = Object.assign({name, description});
-        console.log(newItem);
-        this.props.addRecipe(newItem);
+        //if either are null do not update the state with the new item
+        if (name === '' || description === '') {
+            this.setState({
+                error: !this.state.error,
+            });
+            console.log(this.state.error);
+        } else {
+            //create a new object containing the name and description
+            const newItem = Object.assign({name, description});
+            console.log(newItem);
+            this.props.addRecipe(newItem);
+            this.close();
+        }
     }
 
     render() {
-        const { open, size } = this.state;
+        const { open, size, error } = this.state;
         return (
             <div>
                <Button fluid color='blue' onClick={this.show('small')}>Add Recipe</Button>
@@ -41,9 +54,11 @@ class AddRecipe extends Component {
                         Add a Receipe
                     </Modal.Header>
                     <Modal.Content>
-                        <Form onSubmit={this.handleSubmit}>
+                        <Form onSubmit={this.handleSubmit} error>
                             <Form.Input fluid label='Name' name='name' value={this.state.name} placeholder='Recipe Name' onChange={this.handleChange}/>
-                            <Form.Input fluid label='Description' name='description' value={this.state.description} placeholder='Description'   onChange={this.handleChange}/>
+                            { error ? <Message error header='Error' content='The name cannot be null' /> : null }
+                            <Form.Input fluid label='Description' name='description' value={this.state.description} placeholder='Description'  onChange={this.handleChange}/>
+                            { error ? <Message error header='Error' content='The description cannot be null' /> : null }
                             <Form.Button content='Submit'/>
                             <Form.Button type="button" onClick={this.close}>Close</Form.Button>
                         </Form>
--- src/components/EditRecipe.js
+++ src/components/EditRecipe.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
-import { Button, Modal, Form } from 'semantic-ui-react';
+import { Button, Modal, Form, Message } from 'semantic-ui-react';
 
 class EditRecipe extends Component {
     constructor(props) {
         super(props);
         this.state = {
             open: false,
+            error: false,
             name: this.props.itemName || '',
             description: this.props.itemDescription || '',
         }
@@ -23,14 +24,23 @@ class EditRecipe extends Component {
 
     handleSubmit = () => {
         const { name, description } = this.state;
-        //create an object containing the name and description
-        const newItem = Object.assign({name, description});
-        console.log(newItem);
-        this.props.editItem(newItem, this.props.activeIndex);
+        console.log(name + description);
+        if (name === '' || description === '') {
+            this.setState({
+                error: !this.state.error,
+            });
+            console.log(this.state.error);
+        } else {
+            //create an object containing the name and description
+            const newItem = Object.assign({name, description});
+            console.log(newItem);
+            this.props.editItem(newItem, this.props.activeIndex);
+            this.close();
+        }
     }
 
     render() {
-        const { open, size } = this.state;
+        const { open, size, error } = this.state;
         return (
             <div>
                 <Button color='blue' onClick={this.show('small')}>Edit Item</Button>
@@ -39,9 +49,11 @@ class EditRecipe extends Component {
                         Edit a Receipe
                     </Modal.Header>
                     <Modal.Content>
-                        <Form onSubmit={this.handleSubmit}>
+                        <Form onSubmit={this.handleSubmit} error>
                             <Form.Input fluid label='Name' name='name' value={this.state.name} placeholder='Recipe Name' onChange={this.handleChange}/>
+                            { error ? <Message error header='Error' content='The name cannot be null' /> : null }
                             <Form.Input fluid label='Description' name='description' value={this.state.description} placeholder='Description'   onChange={this.handleChange}/>
+                            { error ? <Message error header='Error' content='The description cannot be left blank.' /> : null }
                             <Form.Button content='Submit'/>
                             <Form.Button type="button" onClick={this.close}>Close</Form.Button>
                         </Form>
