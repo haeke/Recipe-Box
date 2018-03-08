31e513e4239635b740757851fe30f8bd47c5b719
Edwin Rivera <waehake@gmail.com>
2018-03-07 19:57:49 -0500

    updated the readme description for the Recipe-Box Project.

--- README.md
+++ README.md
@@ -2,12 +2,17 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 # Project Name - Recipe-Box
 
-Description - The idea for this project came from FreeCodeCamp. There is a challenge to build this application that lists recipes and ingredients. In this web app, the recipe box that let's a user add a recipe name and a brief description. The data that is entered is stored into firebase so the data is persistent.
+Description - The idea for this project came from FreeCodeCamp. There is a challenge to build an application that lists recipes and ingredients. In this web app, the recipe box let's a user add a recipe name and a brief description. I chose to add the data to a database instead of using local storage so that the list of entered information will be shown to anyone who uses the web app. The data that is entered is stored into firebase so the data is persistent.
 
 # Motivation
-  - The motivation for this application is to get comfortable with creating React components to render data. Additionally, to learn more about storing information using firebase.
+  - The motivation for this application is to get comfortable with creating React components to render data.
+  - Learn more about storing information using firebase.
+  - Work with forms in React to create and edit state.
+  - Continue to work with Semantic-ui-react for responsive web design across multiple devices.
+  - Practice the ability to Create, Read, Update and Delete operations in JavaScript and React.
 
 # Technology Used
+ - JavaScript
  - React
  - Semantic-ui-react
  - Firebase
--- src/components/RecipeBox.js
+++ src/components/RecipeBox.js
@@ -5,83 +5,83 @@ import ShowRecipes from './ShowRecipes';
 import base from '../data/base';
 
 class RecipeBox extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            activeIndex: 0,
-            editRecipeItem: [],
-            recipes: [],
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+        activeIndex: 0,
+        editRecipeItem: [],
+        recipes: [],
+      };
 
-        this.addRecipe = this.addRecipe.bind(this);
-        this.removeItem = this.removeItem.bind(this);
-        this.editItem = this.editItem.bind(this);
-    }
+    this.addRecipe = this.addRecipe.bind(this);
+    this.removeItem = this.removeItem.bind(this);
+    this.editItem = this.editItem.bind(this);
+  }
 
-    componentWillMount() {
-        this.recipesRef = base.syncState('recipes', {
-            context: this,
-            state: 'recipes',
-        });
-    }
+  componentWillMount() {
+    this.recipesRef = base.syncState('recipes', {
+        context: this,
+        state: 'recipes',
+      });
+  }
 
-    componentWillUnmount() {
-        base.removeBinding(this.recipesRef);
-    }
+  componentWillUnmount() {
+    base.removeBinding(this.recipesRef);
+  }
 
-    handleClick = (e, titleProps) => {
-        const { index } = titleProps;
-        const { activeIndex } = this.state;
-        const newIndex = activeIndex === index ? -1 : index;
-       
-        this.setState({ activeIndex: newIndex });
-    }
+  handleClick = (e, titleProps) => {
+      const { index } = titleProps;
+      const { activeIndex } = this.state;
+      const newIndex = activeIndex === index ? -1 : index;
 
-    addRecipe(item) {
-        this.setState({
-            recipes: [...this.state.recipes, item],
-        });
-    }
+      this.setState({ activeIndex: newIndex });
+  };
 
-    removeItem(index) {
-        let {recipes} = this.state;
-        recipes.splice(index, 1);
-        this.setState({
-            recipes: this.state.recipes,
-        });
-    }
+  addRecipe(item) {
+      this.setState({
+          recipes: [...this.state.recipes, item],
+      });
+  }
 
-    editItem(item, activeIndex) {
-        let { recipes } = this.state;
-        this.setState({
-            recipies: recipes[activeIndex] = item,
-        });
-    }
+  removeItem(index) {
+      let { recipes } = this.state;
+      recipes.splice(index, 1);
+      this.setState({
+          recipes: this.state.recipes,
+      });
+  }
 
-    render() {
-        return (
-            <div>
-                <Grid centered computer={16}>
-                    <Grid.Row>
-                        <Grid.Column computer={8} tablet={16} mobile={16}>
-                            <Header as='h1' content='Recipe Box' textAlign='center' />
-                            <Accordion fluid styled>
-                                <AddRecipe addRecipe={this.addRecipe} />
-                                {
-                                    this.state.recipes.length > 0 ?
-                                        <ShowRecipes 
-                                        recipes={this.state.recipes}
-                                        removeItem={this.removeItem}
-                                        editItem={this.editItem}
-                                        addRecipe={this.addRecipe} /> : null
-                                    }
-                            </Accordion>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
-            </div>
-        )
-    }
+  editItem(item, activeIndex) {
+      let { recipes } = this.state;
+      this.setState({
+          recipies: recipes[activeIndex] = item,
+      });
+  }
+
+  render() {
+      return (
+          <div>
+              <Grid centered computer={16}>
+                  <Grid.Row>
+                      <Grid.Column computer={8} tablet={16} mobile={16}>
+                          <Header as='h1' content='Recipe Box' textAlign='center' />
+                          <Accordion fluid styled>
+                              <AddRecipe addRecipe={this.addRecipe} />
+                              {
+                                  this.state.recipes.length > 0 ?
+                                      <ShowRecipes
+                                      recipes={this.state.recipes}
+                                      removeItem={this.removeItem}
+                                      editItem={this.editItem}
+                                      addRecipe={this.addRecipe} /> : null
+                                  }
+                          </Accordion>
+                      </Grid.Column>
+                  </Grid.Row>
+              </Grid>
+          </div>
+      )
+  }
 }
 
 export default RecipeBox;
