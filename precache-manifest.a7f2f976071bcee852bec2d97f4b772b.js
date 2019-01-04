4faa597caf1ad5d3df94cf9de892fe01d2fc6745
waehake <erivera@student.wpunj.edu>
2019-01-03 19:47:37 -0500

    Updates

---
+++ precache-manifest.a7f2f976071bcee852bec2d97f4b772b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "754a38e01e652a56e250",
+    "url": "/Recipe-Box/static/js/runtime~main.754a38e0.js"
+  },
+  {
+    "revision": "f782e38bd6d3bdd82a21",
+    "url": "/Recipe-Box/static/js/main.f782e38b.chunk.js"
+  },
+  {
+    "revision": "c82a228a70a538976697",
+    "url": "/Recipe-Box/static/js/1.c82a228a.chunk.js"
+  },
+  {
+    "revision": "f782e38bd6d3bdd82a21",
+    "url": "/Recipe-Box/static/css/main.95b1bab0.chunk.css"
+  },
+  {
+    "revision": "4fcc6473004cee18fcfda8cdcbc54165",
+    "url": "/Recipe-Box/index.html"
+  }
+];
