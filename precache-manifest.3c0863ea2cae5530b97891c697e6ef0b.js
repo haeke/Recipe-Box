e195d86ae4012f9ed3601c52788d94703497cc16
waehake <erivera@student.wpunj.edu>
2019-02-03 13:06:07 -0500

    Updates

--- precache-manifest.3c0863ea2cae5530b97891c697e6ef0b.js
+++ precache-manifest.3c0863ea2cae5530b97891c697e6ef0b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/Recipe-Box/static/js/runtime~main.754a38e0.js"
   },
   {
-    "revision": "94348227f5ec4d2cf84d",
-    "url": "/Recipe-Box/static/js/main.94348227.chunk.js"
+    "revision": "ac843969a0394df96c74",
+    "url": "/Recipe-Box/static/js/main.ac843969.chunk.js"
   },
   {
     "revision": "c82a228a70a538976697",
     "url": "/Recipe-Box/static/js/1.c82a228a.chunk.js"
   },
   {
-    "revision": "94348227f5ec4d2cf84d",
+    "revision": "ac843969a0394df96c74",
     "url": "/Recipe-Box/static/css/main.710f5365.chunk.css"
   },
   {
-    "revision": "bfbdd53d4164611c46e286b84deff7f0",
+    "revision": "37814f5ceda2d435e0699defbc5de1ea",
     "url": "/Recipe-Box/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/Recipe-Box/precache-manifest.ba8d48304279e7dd334988bf29b1ed67.js"
+  "/Recipe-Box/precache-manifest.3c0863ea2cae5530b97891c697e6ef0b.js"
 );
 
 workbox.clientsClaim();
